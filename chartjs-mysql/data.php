33cb5bf83277f6a3cdb23cd815fd65f534c6a482
sayangsalmah <89141403+sayangsalmah@users.noreply.github.com>
2023-03-22 12:49:28 +0800

    Update data.php

--- chartjs-mysql/data.php
+++ chartjs-mysql/data.php
@@ -3,10 +3,10 @@
 header('Content-Type: application/json');
 
 //database
-define('DB_HOST', '127.0.0.1');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'new_db');
+define('DB_HOST', 'sql12.freemysqlhosting.net');
+define('DB_USERNAME', 'sql12607669');
+define('DB_PASSWORD', 'BxT7Q3Pntl');
+define('DB_NAME', 'sql12607669');
 
 //get connection
 $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
